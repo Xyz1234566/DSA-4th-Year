1cc65c308eaacba01c2aa30a52dec009619ce250
Somansh_verma <somanshverma12@gmail.com>
2026-07-13 10:57:46 +0530

    day1

---
+++ day1/SubarraySum.java
@@ -0,0 +1,23 @@
+public class SubarraySum {
+
+    public static int SubarraySumBrute(int[] nums, int k) {
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int sum = 0;
+
+            for (int j = i; j < nums.length; j++) {
+
+                sum += nums[j];
+
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
